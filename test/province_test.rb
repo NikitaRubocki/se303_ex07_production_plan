aa0967ed6ea6dc0b6dfda93312d9cf0171a8a7ad
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-14 17:31:23 -0700

    Add zero demand test

--- test/province_test.rb
+++ test/province_test.rb
@@ -33,5 +33,11 @@ class ProvinceTest < Minitest::Test
     assert_equal(0, @no_producers.profit)
   end
 
+  def test_province_zero_demand
+    @asia.demand = 0
+    assert_equal(-25, @asia.shortfall)
+    assert_equal(0, @asia.profit)
+  end
+
 
 end
